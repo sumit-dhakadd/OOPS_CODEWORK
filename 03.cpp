50d4be2f0b08ef6f8d785f33a9c4287527ef572f
Sumit Dhakad <smtdhakad2006@gmail.com>
2026-05-06 20:54:10 +0530

    Update 03.cpp

--- 03.cpp
+++ 03.cpp
@@ -1,123 +1,46 @@
 #include<iostream>
 using namespace std;
 
-class Function_OverLoading{
+class Calculation {
 public:
-    Function_OverLoading(){};
-    
-    void Sum(int a , int b){
-        cout << "Sum is :" << a+b <<endl;
+
+    int sum(int a, int b) {
+        return a + b;
     }
-    
-    void Sum(float a , float b){
-        cout << "Sum is :" << a+b <<endl;
+
+    int sum(int a, int b, int c) {
+        return a + b + c;
     }
-    
-    void Sum(float a , int b){
-        cout << "Sum is :" << a+b <<endl;
+
+    float area(float r) {
+        return 3.14 * r * r;
     }
-    
-    void Sum(int a , float b){
-        cout << "Sum is :" << a+b <<endl;
+
+    int area(int l, int b) {
+        return l * b;
     }
-    
-    void Area(int l , int h){
-        cout << "Area of reactangle is : " << l*h <<endl;;
+
+    int volume(int s) {
+        return s * s * s;
     }
-    
-    void Area(int l , float h){
-        cout << "Area of reactangle is : " << l*h << endl;;
+
+    int volume(int l, int b, int h) {
+        return l * b * h;
     }
-    
-    void Area(float l , float h){
-        cout << "Area of reactangle is : " << l*h << endl;;
-    }
-    
-    void Area(float l , int h){
-        cout << "Area of reactangle is : " << l*h << endl;;
-    }
-    
-    void Volume(int l , int b , int h){
-        cout << "Volume of rectangle :" << l*b*h << endl;
-    }
-    
-    void Volume(int l , int b , float h){
-        cout << "Volume of rectangle :" << l*b*h << endl;
-    }
-    
-    void Volume(int l , float b , int h){
-        cout << "Volume of rectangle :" << l*b*h << endl;
-    }
-    
-    void Volume(float l , int b , int h){
-        cout << "Volume of rectangle :" << l*b*h << endl;
-    }
-    
-    void Volume(float l , float b , int h){
-        cout << "Volume of rectangle :" << l*b*h << endl;
-    }
-    
-    void Volume(int l , float b , float h){
-        cout << "Volume of rectangle :" << l*b*h << endl;
-    }
-    
-    void Volume(float l , int b , float h){
-        cout << "Volume of rectangle :" << l*b*h << endl;
-    }
-    
-    void Volume(float l , float b , float h){
-        cout << "Volume of rectangle :" << l*b*h << endl;
-    }
-    
 };
 
-int main(){
-    Function_OverLoading F1;
-    cout <<"Sum Function Calling" << endl;
-    F1.Sum(1,2);
-    cout << endl;
-    F1.Sum(1.3f,2);
-    cout << endl;
-    F1.Sum(1,2.5f);
-    cout << endl;
-    F1.Sum(1.2f,2.6f);
-    cout << endl;
-    
-    cout <<"Area Function Calling" << endl;
-    F1.Area(5,8);
-    cout << endl;
-    F1.Area(5,8.6f);
-    cout << endl;
-    F1.Area(5.5f,8);
-    cout << endl;
-    F1.Area(5.6f,8.3f);
-    cout << endl;
-    
+int main() {
 
-    cout <<"Volume Function Calling" << endl;
-    
-    cout << endl;
-    F1.Volume(5,7,8);
-    cout << endl;
-    F1.Volume(5,8,7.3f);
-    cout << endl;
-    F1.Volume(5,7.6f,8);
-    cout << endl;
-    F1.Volume(5.9f,7,8);
-    cout << endl;
-    F1.Volume(5.6f,7.6f,8);
-    cout << endl;
-    F1.Volume(5.7f,7,8.5f);
-    cout << endl;
-    F1.Volume(5,7.5f,8.4f);
-    cout << endl;
-    F1.Volume(5.1f,7.5f,8.4f);
-    cout << endl;
-  
-    
+    Calculation obj;
+
+    cout<<"Sum = "<<obj.sum(10, 20)<<endl;
+    cout<<"Sum = "<<obj.sum(10, 20, 30)<<endl;
+
+    cout<<"Area of Circle = "<<obj.area(5.0f)<<endl;
+    cout<<"Area of Rectangle = "<<obj.area(4, 6)<<endl;
+
+    cout<<"Volume of Cube = "<<obj.volume(3)<<endl;
+    cout<<"Volume of Cuboid = "<<obj.volume(2, 3, 4)<<endl;
+
+    return 0;
 }
-
-
-
-
-
